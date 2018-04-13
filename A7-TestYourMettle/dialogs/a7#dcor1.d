ec6c46e46f749dd85c453c0e00e7250756b651f6
Argent77 <4519923+Argent77@users.noreply.github.com>
2018-04-13 12:43:10 +0200

    Improved taskmaster dialog

--- A7-TestYourMettle/dialogs/a7#dcor1.d
+++ A7-TestYourMettle/dialogs/a7#dcor1.d
@@ -4,7 +4,7 @@ BEGIN ~a7#dcor1~
 
 IF ~True()~ Intro
   SAY @6000 /* Ah, another clueless adventurer finds its way to the taskmaster. That is good. Guarding the portal is a privilege, but it gets tiresome after a while. */
-  = @6001 /* But introductions first. I am Dogreth the Taskmaster. I would ask for your name, but your soul will be mine soon. Then I will know all about you anyway. */
+  = @6001 /* But introductions first. I am Dogreth the Taskmaster, cornugon of the second circle and the master's right hand. I would ask for your name, but your soul will be mine soon. Then I will know all about you anyway. */
   ++ @6002 /* Are you responsible for the malfunctions of the dungeon maze? */ + Malfunction
   ++ @6003 /* Do you have the vortex cube in your possession? */ + Cube
   IF ~~ EXIT
