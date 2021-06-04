602e3b77550b0231c59cd6fc2a32f5fdc9796dba
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-06-04 20:12:23 +0200

    Add areas to game's debug console

---
+++ A7-TestYourMettle/lua/bgee-append.lua
@@ -0,0 +1,5 @@
+--Test Your Mettle! areas
+{"A7#1FY", "Modron Maze - Modron Foyer"},
+{"A7#1EN", "Modron Maze - Engineering Room"},
+{"A7#1FD", "Modron Maze - Portal Room"},
+{"A7#1WZ", "Modron Maze - Boos Room"},
