86fd74afd73a21c4715c81539b31c7b52abf1bcd
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-06-04 18:45:40 +0200

    Add option for sorcerers to learn the Spacewarp spell

--- A7-TestYourMettle/dialogs/a7#scrl2.d
+++ A7-TestYourMettle/dialogs/a7#scrl2.d
@@ -20,18 +20,22 @@ END
 
 IF ~~ Intro.Known.1
   SAY @10003 /* One section appears to be written with a more practical application in mind. It describes how to create a gravitational field of considerable power for a limited period of time. An intelligent mage or wise priest may learn how to put this effect into an arcane formula. */
-  + ~CheckStatLT(LastTalkedToBy,17,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL)~ + @10010 /* Turn this effect into a mage scroll. */ + Examine.Failed
+  + ~CheckStatLT(LastTalkedToBy,17,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL) !Class(LastTalkedToBy,SORCERER)~ + @10010 /* Turn this effect into a mage scroll. */ + Examine.Failed
+  + ~OR(2) CheckStatLT(LastTalkedToBy,15,INT) LevelLT(LastTalkedToBy,16) Class(LastTalkedToBy,SORCERER)~ + @10010 /* Turn this effect into a mage scroll. */ + Examine.Failed
   + ~CheckStatLT(LastTalkedToBy,17,WIS) Class(LastTalkedToBy,CLERIC_ALL)~ + @10011 /* Memorize this effect as a priest spell. */ + Examine.Failed
-  + ~CheckStatGT(LastTalkedToBy,16,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL)~ + @10010 /* Turn this effect into a mage scroll. */ + Scribe.Mage.Success
+  + ~CheckStatGT(LastTalkedToBy,16,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL) !Class(LastTalkedToBy,SORCERER)~ + @10010 /* Turn this effect into a mage scroll. */ + Scribe.Mage.Success
+  + ~CheckStatGT(LastTalkedToBy,14,INT) LevelGT(LastTalkedToBy,15) Class(LastTalkedToBy,SORCERER)~ + @10013 /* Memorize this effect as a mage spell. */ + Learn.Sorcerer.Success
   + ~CheckStatGT(LastTalkedToBy,16,WIS) Class(LastTalkedToBy,CLERIC_ALL)~ + @10011 /* Memorize this effect as a priest spell. */ + Learn.Cleric.Success
   ++ @10009 /* Put the scroll away. */ EXIT
 END
 
 IF ~~ Intro.Known.2
   SAY @10004 /* This scroll describes how to create a gravitational field of considerable power for a limited period of time. An intelligent mage or wise priest may learn how to put this effect into an arcane formula. */
-  + ~CheckStatLT(LastTalkedToBy,17,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL)~ + @10010 /* Turn this effect into a mage scroll. */ + Examine.Failed
+  + ~CheckStatLT(LastTalkedToBy,17,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL) !Class(LastTalkedToBy,SORCERER)~ + @10010 /* Turn this effect into a mage scroll. */ + Examine.Failed
+  + ~OR(2) CheckStatLT(LastTalkedToBy,15,INT) LevelLT(LastTalkedToBy,16) Class(LastTalkedToBy,SORCERER)~ + @10010 /* Turn this effect into a mage scroll. */ + Examine.Failed
   + ~CheckStatLT(LastTalkedToBy,17,WIS) Class(LastTalkedToBy,CLERIC_ALL)~ + @10011 /* Memorize this effect as a priest spell. */ + Examine.Failed
-  + ~CheckStatGT(LastTalkedToBy,16,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL)~ + @10010 /* Turn this effect into a mage scroll. */ + Scribe.Mage.Success
+  + ~CheckStatGT(LastTalkedToBy,16,INT) OR(2) Class(LastTalkedToBy,MAGE_ALL) Class(LastTalkedToBy,BARD_ALL) !Class(LastTalkedToBy,SORCERER)~ + @10010 /* Turn this effect into a mage scroll. */ + Scribe.Mage.Success
+  + ~CheckStatGT(LastTalkedToBy,14,INT) LevelGT(LastTalkedToBy,15) Class(LastTalkedToBy,SORCERER)~ + @10013 /* Memorize this effect as a mage spell. */ + Learn.Sorcerer.Success
   + ~CheckStatGT(LastTalkedToBy,16,WIS) Class(LastTalkedToBy,CLERIC_ALL)~ + @10011 /* Memorize this effect as a priest spell. */ + Learn.Cleric.Success
   ++ @10009 /* Put the scroll away. */ EXIT
 END
@@ -44,6 +48,11 @@ END
 IF ~~ Scribe.Mage.Success
   SAY @10006 /* As you inspect the notes more closely, you can quickly make out a pattern. Using your own arcane knowledge you are able to transform it into a regular spell scroll without much effort. */
   IF ~~ DO ~SetGlobal("A7#SpacewarpMage","GLOBAL",1) TransformItem("a7#scrl2","a7#mscl2") DisplayString(LastTalkedToBy,@10012)~ EXIT
+END
+
+IF ~~ Learn.Sorcerer.Success
+  SAY @10014 /* After committing the notes to memory you begin to understand the meaning behind the arcane theories. With this knowledge you are able to manipulate the weave well enough to cast this spell at will. */
+  IF ~~ DO ~SetGlobal("A7#SpacewarpMage","GLOBAL",1) DestroyItem("a7#scrl2") AddSpecialAbility("a7#spw1") AddXPObject(LastTalkedToBy,7000)~ EXIT
 END
 
 IF ~~ Learn.Cleric.Success
