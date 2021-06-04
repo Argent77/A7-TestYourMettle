26ade8250ff30dff53775a04fca7f73eba9f55f2
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-06-04 20:13:44 +0200

    Typo

--- A7-TestYourMettle/lua/bgee-append.lua
+++ A7-TestYourMettle/lua/bgee-append.lua
@@ -2,4 +2,4 @@
 {"A7#1FY", "Modron Maze - Modron Foyer"},
 {"A7#1EN", "Modron Maze - Engineering Room"},
 {"A7#1FD", "Modron Maze - Portal Room"},
-{"A7#1WZ", "Modron Maze - Boos Room"},
+{"A7#1WZ", "Modron Maze - Boss Room"},
